0577c51d7825718d3d90db74b10a2926f6dbc8cb
Érico Andrei <erico@simplesconsultoria.com.br>
2014-03-10 11:32:40 -0300

    Preparing release 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 import os
 
-version = '1.0.1.dev0'
+version = '1.0.1'
 long_description = open("README.rst").read() + "\n" + \
     open(os.path.join("docs", "CREDITS.rst")).read() + "\n" + \
     open(os.path.join("docs", "HISTORY.rst")).read()
